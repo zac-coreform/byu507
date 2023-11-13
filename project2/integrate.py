e8cab1d44f261c1da7f46f2ec5d5663f98db123c
Zachary White <zwhite@zacbook.local>
2023-11-12 17:25:51 -0700

    add quad integration tests for deg 1-5

--- project2/integrate.py
+++ project2/integrate.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+import basis
 
 import numpy.polynomial.legendre as lg
 
@@ -7,6 +8,26 @@ def get_gauss_quadrature(n_quad):
     pts = lg.leggauss(n_quad)[0]
     wts = lg.leggauss(n_quad)[1]
     return pts, wts
+
+def get_jacobian(X0, X1):
+    xi_lower = -1
+    xi_upper = 1
+    jacobian = (X1 - X0) / (xi_upper - xi_lower)
+    return jacobian
+
+
+def integrate_by_quadrature(function, x_lower, x_upper, n_quad):
+    quad = get_gauss_quadrature(n_quad)
+    gauss_points = quad[0]
+    gauss_weights = quad[1]
+    jacobian = get_jacobian(x_lower, x_upper)
+    integral = 0
+    for p in range(0, len(gauss_points)): 
+        x = basis.map_xi_to_x(X0=x_lower, X1=x_upper, xi=gauss_points[p])
+        function_val = function(x)
+        partial_integral = function_val * gauss_weights[p] * jacobian
+        integral += partial_integral
+    return integral
 
 class Test_get_gauss_quadrature(unittest.TestCase):
     def test_1quad(self):
@@ -27,3 +48,46 @@ class Test_get_gauss_quadrature(unittest.TestCase):
         TestPoints, TestWeights = get_gauss_quadrature(3)
         self.assertTrue(np.allclose(goldPoints, TestPoints))
         self.assertTrue(np.allclose(goldWeights, TestWeights))
+
+
+class Test_integrate_by_quadrature(unittest.TestCase):
+    def test_linear_polynomial(self):
+        function = lambda x: x + 1
+        goldInt = 2
+        TestInt = integrate_by_quadrature(function=function, x_lower=-1, x_upper=1, n_quad=1)
+        self.assertAlmostEqual(goldInt, TestInt)
+    def test_quadratic_polynomial(self):
+        function = lambda x: x**2
+        goldInt = 2/3
+        TestInt = integrate_by_quadrature(function=function, x_lower=-1, x_upper=1, n_quad=2)
+        self.assertAlmostEqual(goldInt, TestInt)
+    def test_cubic_polynomial(self):
+        function = lambda x: x**3 + 1
+        goldInt = 2
+        TestInt = integrate_by_quadrature(function=function, x_lower=-1, x_upper=1, n_quad=2)
+        self.assertAlmostEqual(goldInt, TestInt)
+    def test_quartic_polynomial(self):
+        function = lambda x: x**4 + 1
+        goldInt = 12/5
+        TestInt = integrate_by_quadrature(function=function, x_lower=-1, x_upper=1, n_quad=3)
+        self.assertAlmostEqual(goldInt, TestInt)
+    def test_quintic_polynomial(self):
+        function = lambda x: x**5 + 1
+        goldInt = 2
+        TestInt = integrate_by_quadrature(function=function, x_lower=-1, x_upper=1, n_quad=3)
+        self.assertAlmostEqual(goldInt, TestInt)
+
+class Test_get_jacobian(unittest.TestCase):
+    def test_biunit_to_biunit(self):
+        goldJacobian = 1
+        TestJacobian = get_jacobian(X0=-1, X1=1)
+        self.assertAlmostEqual(goldJacobian, goldJacobian)
+    def test_biunit_to_unit(self):
+        goldJacobian = 0.5
+        TestJacobian = get_jacobian(X0=0, X1=1)
+        self.assertAlmostEqual(goldJacobian, goldJacobian)
+    def test_biunit_to_nontrivial(self):
+        goldJacobian = 2
+        TestJacobian = get_jacobian(X0=3, X1=7)
+        self.assertAlmostEqual(goldJacobian, goldJacobian)
+
